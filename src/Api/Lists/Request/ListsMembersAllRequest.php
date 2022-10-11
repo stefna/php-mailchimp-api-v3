c7d461ced722b932ae0d619c7375f69b133bccda
arnarp <arnar@stefna.is>
2022-10-11 12:07:57 +0000

    Strict types

--- src/Api/Lists/Request/ListsMembersAllRequest.php
+++ src/Api/Lists/Request/ListsMembersAllRequest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Stefna\Mailchimp\Api\Lists\Request;
 
--- src/Model/Campaign/RSSOptions/SendingSchedule/DailySendingDays.php
+++ src/Model/Campaign/RSSOptions/SendingSchedule/DailySendingDays.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Stefna\Mailchimp\Model\Campaign\RSSOptions\SendingSchedule;
 
