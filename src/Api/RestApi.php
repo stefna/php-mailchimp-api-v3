5e12dad88b53ea9c34e2a6265acec39d8eb925d6
Andreas Sundqvist <andreas@stefna.is>
2023-10-31 15:11:35 +0100

    Fix phpstan issues

--- src/Api/RestApi.php
+++ src/Api/RestApi.php
@@ -102,7 +102,7 @@ abstract class RestApi
 		if (!$className) {
 			$className = get_class($item);
 		}
-		/** @var T $className */
+		/** @var T */
 		return new $className($retData);
 	}
 
@@ -120,7 +120,7 @@ abstract class RestApi
 		$path = $this->getPath(self::ACTION_UPDATE, [$id]);
 		$retData = $this->client->patch($path, $data);
 
-		/** @var T $className */
+		/** @var T */
 		return new $className($retData);
 	}
 
